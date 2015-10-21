b67836b9fd0781edba4ac9b1e7e8b70b1c028d51
Sureyya Uslu <sureyyauslu@gmail.com>
2015-10-21 23:36:39 +0200

    fix the test and add more descriptive spec for showing the passing/failing test

--- test/test.js
+++ test/test.js
@@ -1,13 +1,13 @@
-//test.js file
-'use strict'
+// Simple async test for HTTP 200 response code using supertest
+'use strict';
 
 var request = require("supertest"),
-	app = require("../app").getApp;
+    app = require("../app").getApp;
 
 describe('GET /', function(){
-	it('expects HTTP response 200',function(done){
-		request(app)
-			.get(app)
-			.expect(200,done);
-	});
+  it('expects HTTP response 200', function(done){
+    request(app)
+     .get('/')
+	 .expect(200, done); 
+  });
 });
